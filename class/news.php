a67c620e2828b168322ccb4a130d4fc934d195a6
batisuoc <zhenghengyue@gmail.com>
2019-05-07 21:01:12 +0700

    Update slider

--- class/news.php
+++ class/news.php
@@ -14,6 +14,17 @@ class News extends Database
 		if(!$kq) die('Lỗi trong hàm'.__FUNCTION__.' '.$this->db->error);
 		return $kq;
 	}
+
+	function getTinMoi($from, $sotin, $lang)
+	{
+		$sql = "SELECT idTin, TieuDe, Ngay, TomTat, urlHinh, loaitin.Ten AS TenLT
+				FROM tin, loaitin
+				WHERE tin.idLT = loaitin.idLT AND tin.AnHien = 1 AND tin.lang = '$lang'
+				ORDER BY idTin DESC LIMIT $from, $sotin";
+		$kq = $this->db->query($sql);
+		if(!$kq) die('Lỗi trong hàm'.__FUNCTION__.' '.$this->db->error);
+		return $kq;
+	}
 }
 
 ?>
--- slider.php
+++ slider.php
@@ -1,21 +1,21 @@
 <?php $result = $news->getTinNoiBat(0, 4, $lang); ?>
 <div class="caroufredsel_wrapper caroufredsel_wrapper_small_slider">
 	<ul class="small_slider id-small_slider">
-		<li class="slide">
-			<a href="post.html" title="Nuclear Fusion Closer to Becoming a Reality">
-				<img src='images/samples/690x450/image_09.jpg' alt='img'>
-			</a>
-			<div class="slider_content_box">
-				<ul class="post_details simple">
-					<li class="category"><a href="category_health.html" title="HEALTH">HEALTH</a></li>
-					<li class="date">
-						10:11 PM, Feb 02
-					</li>
-				</ul>
-				<h2><a href="post.html" title="Nuclear Fusion Closer to Becoming a Reality">Nuclear Fusion Closer to Becoming a Reality</a></h2>
-				<p class="clearfix">Maecenas mauris elementum, est morbi interdum cursus at elite imperdiet libero. Proin odios dapibus integer an nulla augue pharetra cursus.</p>
-			</div>
-		</li>
+		<?php while ($row = $result->fetch_assoc() ) { ?>
+			<li class="slide">
+				<a href="#" title="<?= $row['TieuDe'] ?>">
+					<img src='<?= $row['urlHinh'] ?>' alt='img' onerror="this.src='/news/defaultImg.jpg'">
+				</a>
+				<div class="slider_content_box">
+					<ul class="post_details simple">
+						<li class="category"><a href="#" title="<?= $row['TenLT'] ?>"><?= $row['TenLT'] ?></a></li>
+						<li class="date"><?= date('d/m/Y', strtotime($row['Ngay'])) ?></li>
+					</ul>
+					<h2><a href="#" title="<?= $row['TieuDe'] ?>"><?= $row['TieuDe'] ?></a></h2>
+					<p class="clearfix"><?= $row['TomTat'] ?></p>
+				</div>
+			</li>
+		<?php } ?>
 	</ul>
 </div>
 <!-- ./ caroufredsel_wrapper caroufredsel_wrapper_small_slider -->
